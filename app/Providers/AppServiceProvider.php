4fa7ce439ae7d2c80f144207459f23ce098c5833
SmartTamalonggehe <smartspartacus@gmail.com>
2020-12-03 19:31:31 +0900

    Tambah Rupiah

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,21 +29,21 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::directive('currency', function ($expression) {
-            return "<?php echo number_format($expression, 0, ',', '.'); ?>";
-        });
+            return "Rp. <?php echo number_format($expression, 0, ',', '.'); ?>";
+});
 
-        $kategori= Kategori::orderBy('nm_kategori')->get();
-        view()->share('menuKategori', $kategori);
+$kategori= Kategori::orderBy('nm_kategori')->get();
+view()->share('menuKategori', $kategori);
 
-        //compose all the views....
-        view()->composer('*', function ($view) 
-        {
-            if (Auth::check()){
-            $keranjang = Keranjang::orderByDesc('id')->where('status','antri')->where('id_user',Auth::user()->id)->get();
-            //...with this variable
-            $view->with('menuKeranjang', $keranjang);
-            }    
-        });  
+//compose all the views....
+view()->composer('*', function ($view)
+{
+if (Auth::check()){
+$keranjang = Keranjang::orderByDesc('id')->where('status','antri')->where('id_user',Auth::user()->id)->get();
+//...with this variable
+$view->with('menuKeranjang', $keranjang);
+}
+});
 
-    }
+}
 }
--- resources/views/admin/pembayaran/data.blade.php
+++ resources/views/admin/pembayaran/data.blade.php
@@ -1,4 +1,5 @@
-<table id="tabelKu" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+<table id="tabelKu" class="table table-bordered dt-responsive nowrap"
+    style="border-collapse: collapse; border-spacing: 0; width: 100%;">
     <thead>
         <tr>
             <th>No</th>
@@ -14,15 +15,15 @@
             <td>{{ $loop->iteration }}</td>
             <td>{{ $item->tujuan->user->name }}</td>
             <td>{{ $item->bank }}</td>
-            <td>{{ $item->total_bayar }}</td>
+            <td>@currency($item->total_bayar)</td>
             <td>
                 @if ($item->status=="Belum Bayar")
-                    <select name="status" id="status" data-id="{{ $item->id }}" class="form-control">
-                        <option value="{{ $item->status }}" selected>{{ $item->status }}</option>
-                        <option value="Sudah Bayar">Sudah Bayar</option>
-                    </select>
+                <select name="status" id="status" data-id="{{ $item->id }}" class="form-control">
+                    <option value="{{ $item->status }}" selected>{{ $item->status }}</option>
+                    <option value="Sudah Bayar">Sudah Bayar</option>
+                </select>
                 @else
-                    Sudah Bayar
+                Sudah Bayar
                 @endif
             </td>
         </tr>
@@ -32,71 +33,68 @@
 
 
 
-    <!-- Required datatable js -->
-    <script src="{{ asset('adminTools/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('adminTools/plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
-    <!-- Buttons examples -->
-    <script src="{{ asset('adminTools/plugins/datatables/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ asset('adminTools/plugins/datatables/buttons.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('adminTools/plugins/datatables/jszip.min.js') }}"></script>
-    <script src="{{ asset('adminTools/plugins/datatables/pdfmake.min.js') }}"></script>
-    <script src="{{ asset('adminTools/plugins/datatables/vfs_fonts.js') }}"></script>
-    <script src="{{ asset('adminTools/plugins/datatables/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('adminTools/plugins/datatables/buttons.print.min.js') }}"></script>
-    <script src="{{ asset('adminTools/plugins/datatables/buttons.colVis.min.js') }}"></script>
-    <!-- Responsive examples -->
-    {{-- <script src="{{ asset('adminTools/plugins/datatables/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('adminTools/plugins/datatables/responsive.bootstrap4.min.js') }}"></script> --}}
-    <script src="{{ asset('adminTools/assets/pages/jquery.datatable.init.js') }}"></script>
+<!-- Required datatable js -->
+<script src="{{ asset('adminTools/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('adminTools/plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
+<!-- Buttons examples -->
+<script src="{{ asset('adminTools/plugins/datatables/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('adminTools/plugins/datatables/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('adminTools/plugins/datatables/jszip.min.js') }}"></script>
+<script src="{{ asset('adminTools/plugins/datatables/pdfmake.min.js') }}"></script>
+<script src="{{ asset('adminTools/plugins/datatables/vfs_fonts.js') }}"></script>
+<script src="{{ asset('adminTools/plugins/datatables/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('adminTools/plugins/datatables/buttons.print.min.js') }}"></script>
+<script src="{{ asset('adminTools/plugins/datatables/buttons.colVis.min.js') }}"></script>
+<!-- Responsive examples -->
+{{-- <script src="{{ asset('adminTools/plugins/datatables/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('adminTools/plugins/datatables/responsive.bootstrap4.min.js') }}"></script> --}}
+<script src="{{ asset('adminTools/assets/pages/jquery.datatable.init.js') }}"></script>
 
 <script>
-    $('#status').on('change', function(){
-        let value=$(this).val()
-        if (value=="Sudah Bayar") {
-            let href=$(this).data('id')
-            let csrf_token=$('meta[name="csrf_token"]').attr('content');
-            Swal.fire({
-                title: 'Yakin?',
-                text: "Apakah anda yakin pelanggan tersebut telah membayar? Status tidak dapat diubah kembali",
-                type: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: 'danger',
-                cancelButtonColor: 'success',
-                confirmButtonText: 'Yakin',
-                cancelButtonText: 'Batal'
-              }).then((result) => {
-                if (result.value) {
-                    $.ajax({
-                        url: "pembayaran/" + href,
-                        type : "POST",
-                        data : {
-                            '_method' : 'PUT',
-                            '_token' :csrf_token,
-                            'id': href
-                            },
-                        success: function(response) {
-                            console.log(response)
-                            Swal.fire(
-                                        'Diubah!',
-                                        'Data Berhasil Diubah',
-                                        'success'
-                                    )
-                            $('#tampil').load('{{ route("pembayaran.create") }}');
-                        }
-                    })
-                }
-            })
+$('#status').on('change', function() {
+    let value = $(this).val()
+    if (value == "Sudah Bayar") {
+        let href = $(this).data('id')
+        let csrf_token = $('meta[name="csrf_token"]').attr('content');
+        Swal.fire({
+            title: 'Yakin?',
+            text: "Apakah anda yakin pelanggan tersebut telah membayar? Status tidak dapat diubah kembali",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: 'danger',
+            cancelButtonColor: 'success',
+            confirmButtonText: 'Yakin',
+            cancelButtonText: 'Batal'
+        }).then((result) => {
+            if (result.value) {
+                $.ajax({
+                    url: "pembayaran/" + href,
+                    type: "POST",
+                    data: {
+                        '_method': 'PUT',
+                        '_token': csrf_token,
+                        'id': href
+                    },
+                    success: function(response) {
+                        console.log(response)
+                        Swal.fire(
+                            'Diubah!',
+                            'Data Berhasil Diubah',
+                            'success'
+                        )
+                        $('#tampil').load('{{ route("pembayaran.create") }}');
+                    }
+                })
+            }
+        })
 
-            return 0;
-        }
-    })
+        return 0;
+    }
+})
 </script>
 
 <script>
-    $(document).ready( function () {
-        $('#tabelKu').DataTable();
-    } );
+$(document).ready(function() {
+    $('#tabelKu').DataTable();
+});
 </script>
-
-
-
